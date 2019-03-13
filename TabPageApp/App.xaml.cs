e481bbf64cb8b401b8394cd0ffa7673850542ec9
andreivercosa <andreivercosa@gmail.com>
2019-03-12 20:20:29 -0400

    v1

---
+++ TabPageApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace TabPageApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new MainPage(),
+                    new PageTwo()
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
